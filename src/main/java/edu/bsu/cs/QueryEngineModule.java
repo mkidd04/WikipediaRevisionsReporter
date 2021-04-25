520b32bff3cc13aa93bce5f757946101d00a983b
Max Kidd <maxwell.kidd4@gmail.com>
2021-04-25 16:47:00 -0400

    Challenge #1: Changed FakeQueryEngine to QueryEngine

--- src/main/java/edu/bsu/cs/QueryEngineModule.java
+++ src/main/java/edu/bsu/cs/QueryEngineModule.java
@@ -1,8 +1,8 @@
 package edu.bsu.cs;
 
 import com.google.inject.AbstractModule;
-import edu.bsu.cs.model.FakeQueryEngine;
 import edu.bsu.cs.model.QueryEngine;
+import edu.bsu.cs.model.WikipediaQueryEngine;
 
 import java.time.Duration;
 
@@ -10,7 +10,7 @@ public class QueryEngineModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(QueryEngine.class).to(FakeQueryEngine.class);
+        bind(QueryEngine.class).to(WikipediaQueryEngine.class);
         bind(Duration.class).toInstance(Duration.ofSeconds(2));
     }
 }
